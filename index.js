2e00faa3196da892d915e082c6462148f5e9c787
Tara Z. Manicsic <tzmanics@gmail.com>
2020-05-06 13:25:24 -0400

    adds for await and starts stream work

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const readdirp = require('readdirp');
 
 module.exports = {
   onPostBuild: async ({ constants: { PUBLISH_DIR }, inputs, utils }) => {
+    let htmlFiles = [];
     console.log('🔎 Searching for misspelled keywords in:');
 
     for await (const entry of readdirp('.', { fileFilter: '*.html' })) {
-      console.log(`${JSON.stringify(entry.path)}`);
+      htmlFiles.push(entry.path);
     }
+
+    console.log(htmlFiles);
+
+    for await (const file of htmlFiles) {
+      fs.createReadStream(file)
+        .on('error', () => {
+          utils.build.failPlugin(`createReadStream error: `, error);
+        })
+        .on('data', () => {
+          console.log('data');
+        })
+        .on('end', () => {
+          console.log('end');
+        });
+    }
+    /* let fileStream = fs.createReadStream(entry.path);
+    const regex = new RegExp('\\b' + 'Betlify' + '\\b');
+    if (regex.test(fileStream)) {
+      console.log(`thing ${entry.path}`);
+    } */
   },
 };
