bc614885e0a14e80159cc22191c5e1ca9050ed08
Tara Z. Manicsic <tzmanics@gmail.com>
2020-05-15 13:09:37 -0400

    passes wordList JSON and removes logs

--- index.js
+++ index.js
@@ -4,36 +4,12 @@ const { modifyHtml } = require('./modify-html');
 
 module.exports = {
   onPostBuild: async ({ constants: { PUBLISH_DIR }, inputs, utils }) => {
-    let htmlFiles = [];
-    let brandWords = {
-      "Netlify" : ["Nrtlify", "netlify", "Betlify", "netilfy"],
-      "GitHub" : ["gitHib"]
-    };
-
-    console.log('🔎 Searching for misspelled keywords in:');
+    let brandWords = JSON.parse(
+      fs.readFileSync(`${PUBLISH_DIR}${inputs.pathToWordList}`)
+    );
 
     for await (const entry of readdirp(PUBLISH_DIR, { fileFilter: '*.html' })) {
       modifyHtml(`${PUBLISH_DIR}/${entry.path}`, brandWords);
     }
-
-    console.log(htmlFiles);
-
-    /* for await (const file of htmlFiles) {
-      fs.createReadStream(file)
-        .on('error', () => {
-          utils.build.failPlugin(`createReadStream error: `, error);
-        })
-        .on('data', () => {
-          console.log('data');
-        })
-        .on('end', () => {
-          console.log('end');
-        });
-    } */
-    /* let fileStream = fs.createReadStream(entry.path);
-    const regex = new RegExp('\\b' + 'Betlify' + '\\b');
-    if (regex.test(fileStream)) {
-      console.log(`thing ${entry.path}`);
-    } */
   },
 };
--- modify-html.js
+++ modify-html.js
@@ -4,26 +4,25 @@ const parse = require('rehype-parse');
 const stringify = require('rehype-stringify');
 const visit = require('unist-util-visit');
 
-const visitor = (brandWords) => tree => {
-  console.log(brandWords);
-  visit(tree, "text", (node) => {
+const visitor = (brandWords) => (tree) => {
+  visit(tree, 'text', (node) => {
     let word = Object.entries(brandWords).forEach((entry) => {
       const [properSpelling, misspellingsArray] = entry;
       misspellingsArray.forEach((misspelledWord) => {
-        console.log(misspelledWord);
         node.value = node.value.replace(misspelledWord, properSpelling);
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
 
 exports.modifyHtml = (filePath, brandWords) => {
+  console.log('🔎 Searching for misspelled keywords in:', filePath);
   const markup = unified()
-  .use(parse)
-  .use(visitor, brandWords)
-  .use(stringify)
-  .processSync(fs.readFileSync(filePath))
-  .toString();
+    .use(parse)
+    .use(visitor, brandWords)
+    .use(stringify)
+    .processSync(fs.readFileSync(filePath))
+    .toString();
 
   fs.writeFileSync(filePath, markup);
-}
+};
